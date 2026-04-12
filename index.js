bc39ec792bbf800f55868300620dcbcd9cb70775
shencan03 <shencan03@gmail.com>
2026-04-12 16:28:52 +0300

    add buttons with basic display event listener and a little css

--- index.js
+++ index.js
@@ -26,3 +26,20 @@ const operate = (num1, num2, operator) => {
         break
     }
 }
+
+const calcDisplay = document.querySelector(".calc-display")
+
+const calcContainer = document.querySelector(".calc-buttons")
+for (let i = 0; i < 10; i++){
+    const num = document.createElement("button")
+    num.id = `num-${i}`
+    num.textContent = i
+
+    num.addEventListener("click", () => {
+        let num = document.createElement("div")    
+        num.textContent = i
+        calcDisplay.appendChild(num)
+    })
+
+    calcContainer.appendChild(num)
+}
